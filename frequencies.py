d9e33b8db912432684696f0ab56e9389221d0da9
Kamila Shadieva <kamila.shadieva@kcl.ac.uk>
2023-10-05 18:47:42 +0100

    "Add functionalty to frequencies()"

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = {};
+   
+    for i in range (len(items)):
+        items[i] = str(items[i]);
+        frequencies[items[i]] = 1;
+        count = 0;
+        for j in range (len(items)):
+            if items[i] == items[j]:
+                count = count + 1;
+                frequencies[items[i]] = count;
+                
     return frequencies
